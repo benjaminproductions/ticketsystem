bf56313a9a5adc0f22259be41b20203d2b65667b
benjaminproductions <84847659+benjaminproductions@users.noreply.github.com>
2022-04-11 10:10:36 +0200

    added ssl database connection

--- config/database.php
+++ config/database.php
@@ -58,9 +58,19 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+
+
+            // https://stackoverflow.com/a/53078124
+
+            //'options' => extension_loaded('pdo_mysql') ? array_filter([
+            //    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            //]) : [],
+            'options'   => [
+                PDO::MYSQL_ATTR_SSL_CA      => '/var/www/html/ticketsystem/storage/app/ssl/ca-cert.pem',
+                PDO::MYSQL_ATTR_SSL_CERT    => '/var/www/html/ticketsystem/storage/app/ssl/cert.pem',
+                PDO::MYSQL_ATTR_SSL_KEY     => '/var/www/html/ticketsystem/storage/app/ssl/key.pem'
+            ],
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'pgsql' => [
@@ -93,29 +103,7 @@ return [
 
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
-    */
-
     'migrations' => 'migrations',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
 
     'redis' => [
 
